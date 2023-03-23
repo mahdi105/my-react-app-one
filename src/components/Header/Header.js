6db0e60d7f29aee8084c1076b6a971cb2de3dd60
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-03-23 21:37:23 +0600

    Added Header and Footer to the React APP

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <ul className='menu'>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>FAQs</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
